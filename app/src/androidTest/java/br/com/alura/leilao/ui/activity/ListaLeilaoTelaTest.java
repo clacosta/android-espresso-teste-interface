a5ab816422765f3d7ae009d7fb4b909db50df44b
Claudio Costa <claudio@imaginaction.com.br>
2019-10-26 15:12:01 -0300

    feet: 02 - 03 - Refatoração e preparação de ambiente de teste

--- app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
+++ app/src/androidTest/java/br/com/alura/leilao/ui/activity/ListaLeilaoTelaTest.java
@@ -1,22 +1,25 @@
 package br.com.alura.leilao.ui.activity;
 
-import android.support.test.espresso.Espresso;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 public class ListaLeilaoTelaTest {
 
     @Rule
-    public ActivityTestRule activityTestRule =
-            new ActivityTestRule( ListaLeilaoActivity.class, true, true );
+    public ActivityTestRule<ListaLeilaoActivity> activityTestRule =
+            new ActivityTestRule<>(ListaLeilaoActivity.class, true, true);
 
     @Test
     public void deve_AparecerUmLeilao_QuandoCarregarUmLeilaoNaApi() {
-        Espresso.onView(ViewMatchers.withText("Computador"))
-                .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        onView(withText("Carro")).check(matches(isDisplayed()));
     }
+
+
 }
